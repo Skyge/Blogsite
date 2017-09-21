100cb26620dd5ea82e5f808b1d7bc9db08c39389
skyge <18844062006@163.com>
2017-09-21 16:10:56 +0800

    use ICIBA API

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,27 +1,33 @@
-# *-* coding:utf8 *-*
 from django.template.loader import get_template
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
 from datetime import  datetime
 from .models import Article
-import random
-import pickle
 
-pickle_file = open('my_data.pkl','rb')
-quotes = pickle.load(pickle_file)
-Qoute = random.choice(quotes)
+import urllib.request
+import json
+
+url = 'http://open.iciba.com/dsapi'
+page = urllib.request.urlopen(url).read()
+data = page.decode("UTF-8")
+
+data_dict = json.loads(data)
+Qoute = data_dict['content']
+note_Qoute = data_dict['note']
 
 def Homepage(request):
     template = get_template('index.html')
     posts = Article.objects.all()
     now = datetime.now()
     qoute = Qoute
+    note_qoute = note_Qoute
     html = template.render(locals())
     return HttpResponse(html)
 
 def Showpost(request,category):
     template = get_template('post.html')
     qoute = Qoute
+    note_qoute = note_Qoute
     try:
         post = Article.objects.get(category=category)
         if post != None:
