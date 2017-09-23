9ff552fa182a6e6e8ee72c95c062c3b004e4c0e4
skyge <18844062006@163.com>
2017-09-23 13:41:28 +0800

    generate abstract automatically

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,15 +1,28 @@
 from django.db import models
 from django.utils import timezone
 
+import markdown
+
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
-    category = models.CharField(max_length=200,blank=True)
     text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
+    abstract = models.CharField(max_length=200, blank=True)
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+
 
     class Meta:
-        ordering = ('-created_date',)
+        ordering = ('-created_time',)
 
-    def __str__(self):
-        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.abstract:
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+                'markdown.extensions.toc',
+            ])
+            self.abstract = strip_tags(md.convert(self.text))[:54]
+
+        super(Article, self).save(*args, **kwargs)
