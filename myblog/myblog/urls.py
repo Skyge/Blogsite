e613715cf6c8c7f03d22fcb6911d8f5d46228da2
skyge <18844062006@163.com>
2017-10-11 15:05:32 +0800

    Login and logout

--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('blog.urls')),
     url(r'', include('comments.urls')),
+    url(r'^users/', include('users.urls')),
+    url(r'^users/', include('django.contrib.auth.urls')),
 ]
